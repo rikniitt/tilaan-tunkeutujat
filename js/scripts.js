cf11a9026f82fece8f4f7e6b36cb9cdba17c16f7
rikniitt <riku.niittymaki@gmail.com>
2016-06-05 12:44:01 +0300

    Removed futile code and file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-//$(document).ready(function() {
-//    invaders.game.init();
-//}).keyup(function(e) {
-//    invaders.utils.keyhandler.keyup(e.which);
-//}).keydown(function(e) {
-//    invaders.utils.keyhandler.keydown(e.which);
-//}).keypress(function(e) {
-//    invaders.utils.keyhandler.keypress(e.which);
-//});
-
-
 window.onload = function() {
   
     invaders.game.init(); 
